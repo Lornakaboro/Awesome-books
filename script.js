8b4313573db4e033b630368212eb533921bae51e
Lornakaboro <lornakaboro@gmail.com>
2023-02-15 15:30:30 +0300

    Modify code to use one JS class

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
-/* eslint no-unused-vars: 0 */
-/* eslint-disable max-classes-per-file */
-
 // Grab items from html
 
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const addBtn = document.getElementById('addButton');
 const addedBooks = document.getElementById('books');
-const bookHide = document.querySelector('.book-hide');
-
-// create local storage
-
 let bookList;
-class Storage {
-  static addToStorage() {
-    const storage = localStorage.setItem('books', JSON.stringify(bookList));
-    return storage;
-  }
-
-  static getStorage() {
-    const storage = localStorage.getItem('books') === null ? [] : JSON.parse(localStorage.getItem('books'));
-
-    return storage;
-  }
-}
-bookList = Storage.getStorage();
 // Create Classes
 
 class Book {
@@ -33,17 +13,15 @@ class Book {
     this.authorOfBook = authorOfBook;
     this.id = id;
   }
-}
-// Add event listener to add button
-class UI {
+
   static displayData() {
     const displayData = bookList.map((item) => `
-      <div class='individual-book'>
-        <p>${item.titleOfBook}</p>
-        <p>${item.authorOfBook}</p>
-        <button class='rmv-btn'data-id=${item.id}>Remove</button>
-        <hr>
-    </div>`);
+          <div class='individual-book'>
+            <p>${item.titleOfBook}</p>
+            <p>${item.authorOfBook}</p>
+            <button class='rmv-btn'data-id=${item.id}>Remove</button>
+            <hr>
+        </div>`);
     addedBooks.innerHTML = (displayData).join('');
   }
 
@@ -58,16 +36,28 @@ class UI {
         e.target.parentElement.remove();
       }
       const rmvBtn = e.target.dataset.id;
-      UI.removeBookArray(rmvBtn);
-      // Storage.addToStorage(bookList);
+      Book.removeBookArray(rmvBtn);
     });
   }
 
   static removeBookArray(id) {
     bookList = bookList.filter((item) => item.id !== +id);
-    Storage.addToStorage(bookList);
+    Book.addToStorage(bookList);
+  }
+
+  static addToStorage() {
+    const storage = localStorage.setItem('books', JSON.stringify(bookList));
+    return storage;
+  }
+
+  static getStorage() {
+    const storage = localStorage.getItem('books') === null ? [] : JSON.parse(localStorage.getItem('books'));
+    return storage;
   }
 }
+bookList = Book.getStorage();
+
+// Add event listener to add button
 
 let id;
 addBtn.addEventListener('click', (e) => {
@@ -76,15 +66,15 @@ addBtn.addEventListener('click', (e) => {
     id = Math.floor(Math.random() * 10000);
     const bookObj = new Book(`"${title.value}" by`, `, ${author.value}`, id);
     bookList = [...bookList, bookObj];
-    UI.displayData();
-    UI.removeBook();
-    UI.clearValue();
+    Book.displayData();
+    Book.removeBook();
+    Book.clearValue();
 
-    Storage.addToStorage(bookList);
+    Book.addToStorage(bookList);
   }
 });
 
 window.addEventListener('DOMContentLoaded', () => {
-  UI.displayData();
-  UI.removeBook();
+  Book.displayData();
+  Book.removeBook();
 });
