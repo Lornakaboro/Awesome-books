bb409d08a68d4c1431a4ff54d7a7d5ebbf8f31ef
Lornakaboro <lornakaboro@gmail.com>
2023-02-15 17:04:23 +0300

    Add contact-section functionality

--- script.js
+++ script.js
@@ -87,6 +87,8 @@ const addNew = document.querySelector('.add-new');
 const mainTitle = document.querySelector('.main-title');
 const mainTitleHidden = document.querySelector('.main-title-hidden');
 const logo = document.querySelector('.logo');
+const contact = document.querySelector('.contacts')
+const contacts = document.querySelector('.contact-info')
 
 formContainer.style.display = 'none';
 
@@ -103,10 +105,21 @@ lists.addEventListener('click', () => {
   addedBooks.style.display = 'block';
   formContainer.style.display = 'none';
   mainTitle.textContent = 'All awesome books';
+  contacts.style.display = 'none';
 });
 
 addNew.addEventListener('click', () => {
   mainTitleHidden.style.display = 'none';
   addedBooks.style.display = 'none';
   formContainer.style.display = 'flex';
+  contacts.style.display = 'none';
+  
+})
+
+contact.addEventListener('click', () => {
+  contacts.style.display = 'block';
+  formContainer.style.display = 'none';
+  mainTitleHidden.style.display = 'none';
+  
+
 })
